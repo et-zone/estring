9b101c3fbcea4c3db60ae0ae4da4be855b7a2446
guanzhouyi <zhouyi.guan@carsome.com>
2022-09-08 16:07:00 +0800

    byte

--- string.go
+++ string.go
@@ -13,8 +13,19 @@ import (
 from github.com/spf13/cast
  */
 
+
+// zero copy
 func String(b []byte)string{
 	return *(*string)(unsafe.Pointer(&b))
+}
+// zero copy
+func Bytes(s string)(b []byte){
+	bh := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	sh := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	bh.Data = sh.Data
+	bh.Cap = sh.Len
+	bh.Len = sh.Len
+	return b
 }
 
 //only fix base type, not map and struct.
